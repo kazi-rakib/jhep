b0814844951ecb0fd9a95d29e54e9e2aeff59325
gavalian <gavalian@meson.jlab.org>
2017-03-23 16:53:52 -0400

    fixed the issue with finding LZ4 lib

--- jhep-hipo/src/main/java/org/jlab/jhep/hipo/data/HipoEvent.java
+++ jhep-hipo/src/main/java/org/jlab/jhep/hipo/data/HipoEvent.java
@@ -8,7 +8,6 @@ package org.jlab.jhep.hipo.data;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;

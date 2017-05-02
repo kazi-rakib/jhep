0f830d2c027d4ebcf3e272ccfe727cb8aaab337e
Gagik Gavalian <gavalian@Gagiks-MacBook-Air.local>
2017-05-02 14:04:09 -0400

    added fortran example of using the wrapper

--- jhep-hipo/cpp/libcpp/wrapper.cpp
+++ jhep-hipo/cpp/libcpp/wrapper.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include "reader.h"
 
-hipo::reader hipoReader;
+hipo::reader hipo_FORT_Reader;
+hipo::record hipo_FORT_Record;
 
 extern "C" {
 
@@ -10,9 +11,16 @@ extern "C" {
     memcpy(buffer,filename,length);
     buffer[length] = '\0';
     printf("FORTRAN opening file : %s\n", buffer);
-    hipoReader.open(filename);
-    *nrecords = hipoReader.getRecordCount();
+    hipo_FORT_Reader.open(filename);
+    *nrecords = hipo_FORT_Reader.getRecordCount();
     free(buffer);
   }
 
+  void hipo_read_record_(int *record, int *n_events){
+     int pos = (*record) - 1;
+     hipo_FORT_Reader.readRecord(hipo_FORT_Record, pos);
+     int nevt = hipo_FORT_Record.getEventCount();
+     *n_events = nevt;
+  }
+  
 }

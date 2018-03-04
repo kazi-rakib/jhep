3b1ab8145a5823a85e65dc84bac7d268b79bbdbe
gavalian <gavalian@jlab.org>
2018-03-04 10:39:25 -0500

    fixed the script jaw.sh

--- jnp-math/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#*********************************************************
-#---------------------------------------------------------
-# JHEP math CLI interface.
-#---------------------------------------------------------
-java -cp "target/jnp-math-1.0-SNAPSHOT-jar-with-dependencies.jar" org.jlab.jnp.math.cli.MathCli $*
